a1ff2ec6826aafb267546dd9a2393ddd6f2791e4
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 16:46:33 +0530

    Player Start Position is Zero

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,6 +7,9 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Wel-Come to the Snake and Ladder");
+
+            int player_Position = 0;
+            Console.WriteLine("Current Position Of Player is: {0}", + player_Position);
         }
     }
 }
